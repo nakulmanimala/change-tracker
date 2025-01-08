8804f3d506199dfa9b57d39281230a65e5795bb2
Nakul Prabhakaran <nakulmanimala@gmail.com>
2025-01-08 18:46:38 +0530

    code update 6

--- change-checker.py
+++ change-checker.py
@@ -15,7 +15,7 @@ repo = git.Repo('.')
 changed = False
 
 # Get the diff between HEAD and specified commit/tag
-diff = repo.git.diff('HEAD', 'ef27661bc4c4f03ca9a95d55091ed28df705eba4', '--', 'entri/ui/static_design_revamp/north_south/sass/')
+diff = repo.git.diff('HEAD', old_git_tag, '--', 'entri/ui/static_design_revamp/north_south/sass/')
 
 if diff:
     # Changes exist in the directory
