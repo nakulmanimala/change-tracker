1515f2b635df35970d2acf077dc57c98c3a0d02e
Nakul Prabhakaran <nakulmanimala@gmail.com>
2025-01-08 18:07:47 +0530

    code update 1

--- change-checker.py
+++ change-checker.py
@@ -4,13 +4,12 @@ import git
 repo = git.Repo('.')
 changed = False
 
-# Get the diff between two commits/tags
-# Replace 'old_commit' and 'new_commit' with your commit hashes or tag names
-diff = repo.git.diff('old_commit', 'new_commit', '--', 'entri/ui/static_design_revamp/north_south/sass/')
+# Get the diff between HEAD and specified commit/tag
+diff = repo.git.diff('HEAD', '4436b2bbe08f1a18fc25c15e7b73f01cf524e3ec', '--', 'entri/ui/static_design_revamp/north_south/sass/')
 
 if diff:
     # Changes exist in the directory
-    print("There are changes in the sass directory between commits:")
+    print("There are changes in the sass directory between HEAD and specified commit:")
     
     # Parse diff output to show specific changes
     for line in diff.split('\n'):
@@ -20,4 +19,4 @@ if diff:
             changed = True
 
 if not changed:
-    print("No changes detected in the sass directory between commits")
+    print("No changes detected in the sass directory between HEAD and specified commit")
